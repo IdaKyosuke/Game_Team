a85594dd8f29c2b63ac35a91c617f39291869350
IdaKyosuke <hmj2422030@stu.o-hara.ac.jp>
2025-09-11 12:19:12 +0900

    オンラインテスト用のFPSゲーム。

---
+++ Inventory_Test/Assets/Scripts/Weapon/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	[SerializeField] float m_speed;
+	[SerializeField] float m_deleteTime;	// Á‚¦‚é‚Ü‚Å‚ÌŠÔ
+	private Rigidbody m_rb;
+	private float m_count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		// ËŒ‚
+        m_rb = GetComponent<Rigidbody>();
+		Vector3 vec = transform.parent.transform.forward;
+		m_rb.AddForce (vec * m_speed);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+		m_count += Time.deltaTime;
+		if(m_count >= m_deleteTime)
+		{
+			Destroy(this.gameObject);
+		}
+    }
+}
