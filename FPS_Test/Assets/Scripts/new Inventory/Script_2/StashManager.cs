92f5a466abf366725da028e6912473f60cf0b327
IdaKyosuke <hmj2422030@stu.o-hara.ac.jp>
2026-02-04 15:41:30 +0900

    アイテム関連の音をつけた。

--- FPS_Test/Assets/Scripts/new Inventory/Script_2/StashManager.cs
+++ FPS_Test/Assets/Scripts/new Inventory/Script_2/StashManager.cs
@@ -1244,6 +1244,7 @@ public class StashManager : MonoBehaviourPunCallbacks
 
 			// アイテムをスタッシュに並べる
 			CheckGrid(GridType.Inventory, g, false, true, true, false);
+			g.GetComponent<Item_Object>().FinishSet();
 
 			index++;
 		}
